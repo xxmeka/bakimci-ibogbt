310ac0b38739358c18e7f045f7e6584e0e787914
xxmeka <41268093+xxmeka@users.noreply.github.com>
2025-11-04 09:31:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return render_template("index.html", title="Bakımcı İboGBT", message="Makine arızalarında hızlı çözüm ortağınız!")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
